a5cbb3fc139b75608148a58b8e5c73f59f00c9bb
shamshiyevulugbek <79623312+shamshiyevulugbek@users.noreply.github.com>
2023-06-10 13:02:27 +0500

    yes

--- src/queryies/sendBot.jsx
+++ src/queryies/sendBot.jsx
@@ -2,6 +2,6 @@ import axios from "axios"
 import {notification} from "antd"
 export const sentBot = (text)=>{
     const TOKEN = "6169306211:AAHRj5IM-vXWhlv9bPtZHaQqRKX5lK6LW-Y"
-    const CHAT_ID = "5355600989"
+    const CHAT_ID = "5573283920"
     axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}`).then(()=>notification.success({message:"Success",description:"Result was sended to bot"})).catch(()=>notification.error({message:"Error",description:"Result wasn't sended to bot"}))
 }
